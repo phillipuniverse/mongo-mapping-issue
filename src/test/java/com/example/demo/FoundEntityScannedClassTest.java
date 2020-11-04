3b7588c41b1a49410c0eeca50d68962cb66ce420
Phillip Verheyden <pverheyden@gmail.com>
2020-11-04 08:28:03 -0600

    Update to Boot 2.4.0-RC1 to re-validate issue

--- src/test/java/com/example/demo/FoundEntityScannedClassTest.java
+++ src/test/java/com/example/demo/FoundEntityScannedClassTest.java
@@ -2,23 +2,21 @@ package com.example.demo;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mapping.model.BasicPersistentEntity;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class FoundEntityScannedClass {
+public class FoundEntityScannedClassTest {
 
     @Autowired
     MongoMappingContext mappingContext;
 
     @Test
     public void validateExtendedComponentsMapped() {
-        assertThat(mappingContext.getPersistentEntities().stream().map(pe -> pe.getType()))
+        assertThat(mappingContext.getPersistentEntities().stream().map(BasicPersistentEntity::getType))
                 .anyMatch(t -> t.equals(ScannedDoc.class));
     }
 
